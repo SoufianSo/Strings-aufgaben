095a679771d71dd3dc5ac04e4a54c5737d8ac794
Soufian <soufian.souilm@gmail.com>
2023-10-24 15:16:33 +0200

    Stringsaufgaben

---
+++ assets/mains.js
@@ -0,0 +1,35 @@
+const text = "          halloleute                 ";
+console.log(text.trim());
+// Variablen erstellen Aufgabe 1-1
+let firstName = "Soufian";
+let lastName = "Souilm";
+let fullname = "soufiansouilm";
+
+let firstNameLength = firstName.length;
+console.log("länge von firstname:", firstNameLength);
+let lastNamelength = lastName.length;
+console.log("länge von firstname:", firstNameLength);
+
+let fullnamelenght = fullname.length;
+console.log("fullname:", fullnamelenght);
+// Variablen erstellen Aufgabe 1-2
+const txt =
+  "How inappropriate to call this planet Earth, when clearly it is Ocean.";
+const positionH = txt.indexOf("h");
+console.log("Position von 'h':", positionH);
+
+const positionEarth = txt.indexOf("Earth");
+console.log("Position von 'Earth':", positionEarth);
+
+const positionMoon = txt.indexOf("Moon");
+console.log("Position von 'Moon':", positionMoon);
+// Variablen erstellen Aufgabe 1-3
+let txt2 =
+  "Blue, green, grey, white, or black; smooth, ruffled, or mountainous; that ocean is not silent.";
+const positionSemicolon = txt2.indexOf(";");
+console.log("Position des Zeichens ';':", positionSemicolon);
+
+const positionGreen = txt2.indexOf("Green");
+console.log("Position der Zeichenfolge 'green':", positionGreen);
+const positionBlue = txt2.indexOf("blue");
+console.log("Position der Zeichenfolge 'blue':", positionBlue);
